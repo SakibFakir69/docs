fadb1a6bda969849f866a25a08fd1cb09a5a7e5c
Sakib Fakir <FakirSakib22232@gmail.com>
2025-10-30 17:23:43 +0800

    useState - ( handle form submission use useState , inside useState 4 object ( name,email , password , number ) , ...prev use to copy previous value , then insert new form value

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,69 @@
-import React from "react";
-import useWindowSize from "./hooks/custom/useWindowSize";
+import React, { useState } from "react";
 
 function App() {
+  const [db, setDB] = useState([]);
 
-  const {postion} = useWindowSize();
-  const {height,width} = postion;
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+    number: "",
+  });
 
-  console.log(postion)
+  // handel from
 
-  
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    console.log(name, value);
 
-  return <div>
+    setForm((prev) => ({ ...prev, [name]: value }));
+    //// copy prev value on from ( copy object)
+  };
 
-    <p>height: {height} - width: {width}</p>
-    <div style={{height:height-100 , width:width-100, border:"2px solid red"}} >
+  const handleFormSubmission = (e) => {
+    // target get -> target element
+    // value -> get element filed value
 
+    e.preventDefault();
+    setForm(form);
+    setDB((prev) => [...prev, form]);
+    // copy prev data , and add new from data 
+
+    console.log(form, "from");
+  };
+  console.log(db);
+
+  return (
+    <div>
+      <form onSubmit={handleFormSubmission}>
+        <input
+          name="name"
+          value={form.name}
+          onChange={handleChange}
+          placeholder="Name"
+        />
+        <input
+          name="email"
+          value={form.email}
+          onChange={handleChange}
+          placeholder="Email"
+        />
+        <input
+          name="password"
+          value={form.password}
+          onChange={handleChange}
+          placeholder="Password"
+        />
+        <input
+          name="number"
+          value={form.number}
+          onChange={handleChange}
+          placeholder="Number"
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
-
-
-  </div>;
+  );
 }
 
 export default App;
