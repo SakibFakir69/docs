bbeff546c3da5a0121652baf2104092a9e24ab9a
Sakib Fakir <FakirSakib22232@gmail.com>
2025-10-30 18:18:25 +0800

    useEffect - ( useEffet run on side effect but we can re-run to set useEffect depedency and in use effect we can remove funct ion then component unmount or removed from dom

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+
+
+// Ngrok
+// Ngrok is a reverse proxy that create secure tunnel from public endpoint to localy running services
+// we can share share our development to client and other locally (expose our local development to internet)
+// Ngrok assign public url to our localhost
+
+
+import React, { useEffect, useState } from 'react'
+
+function App() {
+
+  const [isActive , setIsActive ] = useState(false);
+
+
+  useEffect(()=>{
+
+    if(!isActive)
+    {
+      return "User are not Active"
+    }
+
+   
+    const apiFetch = ()=>{
+       const res = await fetch();
+    const data = await res.json();
+    // fetch data after user have 
+    }
+    apiFetch();
+    // call api fetch
+
+
+    const interValId = ()=> setInterval(apiFetch,5000);
+
+
+
+
+    // clean up 
+    /// clean up remove from dom 
+
+    return ()=> clearInterval(interValId);
+
+
+
+
+  },[isActive]);
+  /// re-run if use isActive change 
+
+
+
+
+
+
+
+
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default App
