5948ce2c60a28f269dab971090fd500c0263f9fd
Sakib Fakir <FakirSakib22232@gmail.com>
2025-11-03 11:21:05 +0800

    n-last- ( use can select element based on number n-last-3 )

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
-
-
-
-
-import React from 'react'
+import React from "react";
 
 function App() {
-
-
-
-
-
   return (
     <div>
+      <button className="bg-yellow-300 text-white hover:bg-sky-400 w-30 p-2 h-10">
+        Hover me
+      </button>
+      <button className="p-3 w-30 border focus:outline-4">Focus</button>
 
-  <button className='bg-yellow-300 text-white hover:bg-sky-400 w-30 p-2 h-10'>Hover me</button>
-      <button className='p-3 w-30 border focus:outline-4'>Focus</button>
+
+
+      <div>
+         {
+          [1,2,3,5,6,7].map((item,key)=>(<div className="even:bg-red-400 first:
+          p-2 odd:bg-green-300" key={key}>
+
+            {item}
+
+          </div>))
+         }
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
