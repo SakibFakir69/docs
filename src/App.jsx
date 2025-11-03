89bdb15ef5d0b243585c45c894a012f79c5355b2
Sakib Fakir <FakirSakib22232@gmail.com>
2025-11-03 15:25:47 +0800

    mui component - ( inputfield ,switch ,slider,button , button group,rating ,checkbox)

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,57 @@
 import React from 'react'
 
 import Button from '@mui/material/Button'
+import ButtonGroup from '@mui/material/ButtonGroup'
+import Checkbox from '@mui/material/Checkbox'
+import Fab from '@mui/material/Fab'
+import Rating from '@mui/material/Rating'
+import Slider from '@mui/material/Slider'
+import Switch from '@mui/material/Switch'
+import TextField from '@mui/material/TextField'
 
 function App() {
   return (
     <div>
-      <Button />
+      <Button  variant='text'>text</Button>
+      <Button variant='outlined'>Text2</Button>
+      <Button variant='contained'>Text3</Button>
+      <Button disabled>Disable</Button>
+      <Button color='primary'>Color</Button>
+
+      <Button className='text-white!' sx={{backgroundColor:"red"}}  >Delete</Button>
+
+      <ButtonGroup size='medium' color='secondary' variant='contained' >
+        <Button>One</Button>
+          <Button>Two</Button>
+            <Button>Three</Button>
+      </ButtonGroup>
+
+      {/* check  box */}
+
+      <Checkbox  size='medium' color='error' defaultChecked/>
+      <Checkbox  size='medium' color='info'/>
+
+      {/* fab */}
+
+      <Fab color='primary'>
+        Add+
+
+      </Fab>
+      <Fab color='secondary'>
+        Edit
+      </Fab>
+
+      {/* rating */}
+
+      <Rating  name='rating' value={3.4} precision={1} readOnly/>
+      <Rating name='rating' value={5} precision={0.5}/>
+
+      <Slider name='slider' defaultValue={10} step={null} marks={''}/>
+      <Switch defaultChecked color='primary' size='medium'/>
       
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+<TextField id="filled-basic" label="Filled" variant="filled" />
+<TextField id="standard-basic" label="Standard" variant="standard" />
       
     </div>
   )
