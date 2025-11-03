a154ba88fd8dbd846f71efcbe32b6abc48b30d2e
Sakib Fakir <FakirSakib22232@gmail.com>
2025-11-03 12:06:25 +0800

    flex and grid - ( flex work row or  column and grid work both row and column )

--- src/App.jsx
+++ src/App.jsx
@@ -12,14 +12,46 @@ function App() {
 
       <div>
          {
-          [1,2,3,5,6,7].map((item,key)=>(<div className="even:bg-red-400 first:
-          p-2 odd:bg-green-300" key={key}>
+          [1,2,3,5,6,7].map((item,key)=>(<div className=" first:
+          p-2 odd:bg-green-300 nth-last-[2]:underline invalid:border-pink-500" key={key}>
 
             {item}
 
           </div>))
          }
       </div>
+
+      {/* responisve */}
+
+
+      <div className="border w-full md:w-1/2  flex  hover:bg-gray-50  text-wrap">
+
+        <div className="h-20 w-20 border">
+
+        </div>
+
+        <p className="text-center md:text-left xl:text-red-400 indent-2.5">Title</p>
+        <p className="text-center text-green-400 md:text-red-400 lg:text-yellow-300 xl:text-black 2xl:bg-green-300 font-sans leading-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis amet quia natus debitis sequi, illo provident molestias voluptas nam enim.</p>
+
+
+      </div>
+
+     <div className="grid grid-cols-12 gap-3">
+  {[1, 2, 3, 4, 5].map((item, key) => (
+    <div
+      key={key}
+      className={`border ${key === 0 ? 'col-span-6' : 'col-span-2'}`}
+    >
+      <p>{item}</p>
+    </div>
+  ))}
+</div>
+
+
+
+
+
+
     </div>
   );
 }
